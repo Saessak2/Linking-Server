a6625c0e8356b9b6a7860d3875d44658143a97f8
macbook <eunbin@eunbiniui-MacBookAir.local>
2023-03-13 22:46:29 +0900

    first commit

---
+++ src/main/java/com/linking/LikingApplication.java
@@ -0,0 +1,13 @@
+package com.linking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikingApplication.class, args);
+	}
+
+}
