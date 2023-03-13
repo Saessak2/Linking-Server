7959e051bddc7bd9271ed63c569b96104914fdb4
ChoiHyemin <mare137042@gmail.com>
2023-03-13 23:09:58 +0900

    [style] proj name fixed

--- src/main/java/com/linking/LinkingApplication.java
+++ src/main/java/com/linking/LinkingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LikingApplication {
+public class LinkingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LikingApplication.class, args);
+		SpringApplication.run(LinkingApplication.class, args);
 	}
 
 }
