ac1e4f86ba9fcf177225a6ad3ef4a323132329a4
BINMUXIZ <leb001122@icloud.com>
2023-04-29 17:56:03 +0900

    .

--- src/main/java/com/linking/page/dto/PageIdRes.java
+++ src/main/java/com/linking/page/dto/PageIdRes.java
@@ -1,17 +1,15 @@
 package com.linking.page.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 public class PageIdRes {
 
-    private Long gropupId;
+    private Long groupId;
     private Long pageId;
 
-    public PageIdRes(Long gropupId, Long pageId) {
-        this.gropupId = gropupId;
+    public PageIdRes(Long groupId, Long pageId) {
+        this.groupId = groupId;
         this.pageId = pageId;
     }
 }
