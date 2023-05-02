3f0082bf7dc81c6e01ea07b95307ed6147e874d6
BINMUXIZ <leb001122@icloud.com>
2023-05-02 20:44:38 +0900

    [ADD] NotificationRepository test
    delete noti
    findByAllUser

--- src/main/java/com/linking/notification/persistence/NotificationRepository.java
+++ src/main/java/com/linking/notification/persistence/NotificationRepository.java
@@ -1,11 +1,20 @@
 package com.linking.notification.persistence;
 
 import com.linking.notification.domain.Notification;
+import com.linking.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
+    @Query("SELECT n FROM Notification n WHERE n.createdDate < :threshold")
+    List<Notification> findAllAgoMonth(@Param("threshold") LocalDate threshold);
 
+    List<Notification> findAllByUser(User user);
 }
--- src/test/java/com/linking/notification/persistence/NotificationRepositoryTest.java
+++ src/test/java/com/linking/notification/persistence/NotificationRepositoryTest.java
@@ -5,15 +5,16 @@ import com.linking.project.domain.Project;
 import com.linking.project.persistence.ProjectRepository;
 import com.linking.user.domain.User;
 import com.linking.user.persistence.UserRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
-import static org.assertj.core.api.Assertions.*;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 
 @DataJpaTest
@@ -31,30 +32,39 @@ class NotificationRepositoryTest {
     @Test
     @Rollback(value = false)
     void save() {
-        User user = userRepository.getReferenceById(1L);
+        User user = userRepository.getReferenceById(4L);
         Project project = projectRepository.getReferenceById(3L);
-        Notification notification = Notification.builder()
-                .user(user)
-                .project(project)
-                .priority("ALL") // NO_MAIL
-                .sender("서민정")
-                .build();
-        Notification save1 = notificationRepository.save(notification);
-        Notification notification2 = Notification.builder()
-                .user(user)
-                .project(project)
-                .priority("NO_MAIL")
-                .sender("서민정")
-                .build();
-        Notification save2 = notificationRepository.save(notification2);
 
-        assertThat(notification).isEqualTo(save1);
-        assertThat(notification2).isEqualTo(save2);
+        for (int i = 0; i < 8; i++) {
+            Notification notification = new Notification(user, project, "서민정", "ALL");
+            notificationRepository.save(notification);
+        }
+//        Notification notification2 = Notification.builder()
+//                .user(user)
+//                .project(project)
+//                .priority("NO_MAIL")
+//                .sender("서민정")
+//                .build();
+//        Notification save2 = notificationRepository.save(notification2);
+
+//        assertThat(notification).isEqualTo(save1);
+//        assertThat(notification2).isEqualTo(save2);
     }
 
     @Test
     @Rollback(value = false)
-    void delete() {
-
+    void delete() {  // 한달이 지난 알람 삭제
+        List<Notification> notifications = notificationRepository.findAllAgoMonth(LocalDate.now().minusMonths(1));
+        notificationRepository.deleteAll(notifications);
     }
+
+    @Test
+    @Rollback
+    void findAllByUser() {
+        User user = userRepository.getReferenceById(4L);
+        List<Notification> allByUser = notificationRepository.findAllByUser(user);
+        Assertions.assertEquals(8, allByUser.size());
+    }
+
+
 }
