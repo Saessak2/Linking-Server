0a96ed6af3c2e07a043a558305d0b0d7df3047a8
BINMUXIZ <leb001122@icloud.com>
2023-05-05 18:50:36 +0900

    [ADD] firebase admin

---
+++ src/main/generated/com/linking/assign/persistence/AssignMapperImpl.java
@@ -0,0 +1,31 @@
+package com.linking.assign.persistence;
+
+import com.linking.assign.domain.Assign;
+import com.linking.assign.dto.AssignRes;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-05-05T18:25:16+0900",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 11.0.16.1 (Oracle Corporation)"
+)
+@Component
+public class AssignMapperImpl implements AssignMapper {
+
+    @Override
+    public List<AssignRes> toResDto(List<Assign> assignList) {
+        if ( assignList == null ) {
+            return null;
+        }
+
+        List<AssignRes> list = new ArrayList<AssignRes>( assignList.size() );
+        for ( Assign assign : assignList ) {
+            list.add( toResDto( assign ) );
+        }
+
+        return list;
+    }
+}
---
+++ src/main/generated/com/linking/page/persistence/PageMapperImpl.java
@@ -0,0 +1,13 @@
+package com.linking.page.persistence;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-05-05T18:25:16+0900",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 11.0.16.1 (Oracle Corporation)"
+)
+@Component
+public class PageMapperImpl implements PageMapper {
+}
