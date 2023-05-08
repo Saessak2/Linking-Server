4e633985d89f55d183adb47cc95e5c59156818bf
BINMUXIZ <leb001122@icloud.com>
2023-05-08 21:42:39 +0900

    .

--- src/main/java/com/linking/notification/service/FCMService.java
+++ src/main/java/com/linking/notification/service/FCMService.java
@@ -19,48 +19,48 @@ public class FCMService {
     private final FirebaseMessaging firebaseMessaging;
     private final UserRepository userRepository;
 
-    public void sendNotificationByToken(FCMReq req) {
-
-        User user = userRepository.findById(req.getTargetUserId())
-                .orElseThrow(() -> new NoSuchElementException(ErrorMessage.NO_USER));
-
-        if (user.getFcmToken() == null) {
-            log.info("유저의 firebase Token이 존재하지 않습니다. targetUserId = {}", req.getTargetUserId());
-            return;
-        }
-
-        Notification notification = Notification.builder()
-                .setTitle(req.getTitle())
-                .setBody(req.getBody())
-                .build();
-
-        ApnsConfig apnsConfig = ApnsConfig.builder()
-                .setAps(Aps.builder().setBadge(42).build())
-                .putHeader("apns-priority", "5")
-                .build();
-
-        WebpushConfig webpushConfig = WebpushConfig.builder()
-                .putHeader("Urgency", "normal")
-                .build();
-
-//        FcmOptions fcmOptions = FcmOptions.builder()
-//                .setAnalyticsLabel()
+//    public void sendNotificationByToken(FCMReq req) {
+//
+//        User user = userRepository.findById(req.getTargetUserId())
+//                .orElseThrow(() -> new NoSuchElementException(ErrorMessage.NO_USER));
+//
+//        if (user.getFcmToken() == null) {
+//            log.info("유저의 firebase Token이 존재하지 않습니다. targetUserId = {}", req.getTargetUserId());
+//            return;
+//        }
+//
+//        Notification notification = Notification.builder()
+//                .setTitle(req.getTitle())
+//                .setBody(req.getBody())
 //                .build();
 //
-        Message message = Message.builder()
-                .setToken(user.getFcmToken())
-                .setNotification(notification)
-                .putAllData(req.getData())
-//                .setFcmOptions()
-                .build();
-
-        try {
-            String response = firebaseMessaging.send(message);
-            log.info("Successfully sent message: {}", response);
-
-        } catch (FirebaseMessagingException e) {
-            e.printStackTrace();
-            log.info("Error sending message", req.getTargetUserId());
-        }
-    }
+//        ApnsConfig apnsConfig = ApnsConfig.builder()
+//                .setAps(Aps.builder().setBadge(42).build())
+//                .putHeader("apns-priority", "5")
+//                .build();
+//
+//        WebpushConfig webpushConfig = WebpushConfig.builder()
+//                .putHeader("Urgency", "normal")
+//                .build();
+//
+////        FcmOptions fcmOptions = FcmOptions.builder()
+////                .setAnalyticsLabel()
+////                .build();
+////
+//        Message message = Message.builder()
+////                .setToken(user.getFcmToken())
+//                .setNotification(notification)
+//                .putAllData(req.getData())
+////                .setFcmOptions()
+//                .build();
+//
+//        try {
+//            String response = firebaseMessaging.send(message);
+//            log.info("Successfully sent message: {}", response);
+//
+//        } catch (FirebaseMessagingException e) {
+//            e.printStackTrace();
+//            log.info("Error sending message", req.getTargetUserId());
+//        }
+//    }
 }
