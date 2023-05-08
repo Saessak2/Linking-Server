9bea22e2b13b5fcb692862164f12d2acff6454d8
BINMUXIZ <leb001122@icloud.com>
2023-05-09 04:51:01 +0900

    [add] 매핑되지 않은 필드 무시

--- src/main/java/com/linking/user/persistence/UserMapper.java
+++ src/main/java/com/linking/user/persistence/UserMapper.java
@@ -4,12 +4,16 @@ import com.linking.user.domain.User;
 import com.linking.user.dto.UserDetailedRes;
 import com.linking.user.dto.UserSignUpReq;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
-@Mapper(componentModel = "spring")
+@Mapper(
+        componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
 public interface UserMapper {
 
     UserDetailedRes toDto(User user);
