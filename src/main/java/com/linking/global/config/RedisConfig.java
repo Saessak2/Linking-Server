31f392f0db9a8bec908d065e95777c2b1d2bae84
BINMUXIZ <leb001122@icloud.com>
2023-05-23 18:15:06 +0900

    .

--- src/main/java/com/linking/global/config/RedisConfig.java
+++ src/main/java/com/linking/global/config/RedisConfig.java
@@ -2,7 +2,9 @@ package com.linking.global.config;
 
 import com.linking.global.common.UserCheck;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
